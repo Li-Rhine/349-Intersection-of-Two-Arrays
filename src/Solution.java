29b908830a46ceb72ded5d18d919eda63cec32cd
dahai.li <dahai.li@china.zhaogang.com>
2020-06-10 18:58:23 +0800

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+class Solution {
+    // 349号问题
+    public int[] intersection(int[] nums1, int[] nums2) {
+
+        TreeSet<Integer> set = new TreeSet<>();
+        for (int num: nums1) {
+            set.add(num);
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int num: nums2) {
+            if (set.contains(num)) {
+                list.add(num);
+                set.remove(num);
+            }
+        }
+
+        int[] res = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+
+
+    // 350号问题
+    public int[] intersect(int[] nums1, int[] nums2) {
+
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int num: nums1)  {
+            if (!map.containsKey(num)) {
+                map.put(num, 1);
+            }else {
+                map.put(num, map.get(num) + 1);
+            }
+        }
+    }
+}
