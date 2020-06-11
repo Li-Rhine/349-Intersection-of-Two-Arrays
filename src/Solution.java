671d1a43dbf39e485c918d60820546e48d57c758
dahai.li <dahai.li@china.zhaogang.com>
2020-06-11 16:36:29 +0800

    求交集

--- src/Solution.java
+++ src/Solution.java
@@ -31,12 +31,28 @@ class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
 
         TreeMap<Integer, Integer> map = new TreeMap<>();
-        for (int num: nums1)  {
+        for (int num : nums1) {
             if (!map.containsKey(num)) {
                 map.put(num, 1);
-            }else {
+            } else {
                 map.put(num, map.get(num) + 1);
             }
         }
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int num : nums2) {
+            if (map.containsKey(num)) {
+                list.add(num);
+                map.put(num, map.get(num) - 1);
+                if (map.get(num) == 0) {
+                    map.remove(num);
+                }
+            }
+        }
+
+        int[] res = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+        return res;
     }
 }
